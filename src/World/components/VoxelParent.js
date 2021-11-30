2786c447025ed512b40dc8d285e8759f7e7e6d7c
Andrew Richerson <ajorigami1@gmail.com>
2021-11-30 03:25:57 -0600

    Build project for GH Pages

--- src/World/components/VoxelParent.js
+++ src/World/components/VoxelParent.js
@@ -1,9 +1,10 @@
-import { Object3D } from 'three';
+import { Object3D, Group } from 'three';
 import { createCoordinateBasis } from './coordinate-basis';
 import { ObjectMap } from '../systems/ObjectMap';
 const coordBasis = createCoordinateBasis();
 const voxelMap = new ObjectMap();
 
+// TODO: rename to VoxelGroup and either extend from Group or just return a Group
 class VoxelParent extends Object3D {
     constructor() {
         super();
